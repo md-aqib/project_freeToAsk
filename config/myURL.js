19ab16f4f0e4a4d9c65601559b887daaba2c66ff
aquibmax <mdaquib327@gmail.com>
2019-10-17 00:38:06 +0530

    2nd commit

---
+++ config/myURL.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURL: "mongodb://localhost",
+    secret: "mystrongsecret"
+  };
--- server.js
+++ server.js
@@ -2,3 +2,29 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const myURL = require('./config/myURL') 
+
+
+
+const app = epress()
+
+//Middleware for bodyparser
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+//mongoDB configuration
+const db = require("./config/myURL").mongoURL; 
+
+
+//Attempt to connect to database
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log(err));
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App is running at ${port}`));
