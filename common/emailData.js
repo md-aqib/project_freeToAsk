e9c27cb4a9879f16359a8bbc76d935ddcaf1ecae
Mohd Aqib <Your>
2019-11-19 15:14:54 +0530

    changes in ejs file

--- common/emailData.js
+++ common/emailData.js
@@ -1,6 +1,6 @@
 // predefined text
 let contact = `If you have any questions, just call to us on 8840624226 — we're always happy to help you out.`;
-let sign = `Regards,<br>Team Aqib`;
+let sign = `Regards,Team Aqib`;
 
 exports.welcomeEmail = (name) => {
     return {
@@ -10,6 +10,6 @@ exports.welcomeEmail = (name) => {
         Your account is all set and is ready for use. Please login with the registered credentials.
         We are always there to help you.`,
         contact: contact,
-        sign: sign
+        sign: `${sign}`
     };
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const myURL = require('./config/myURL')
 const routes = require('./routes/routes')
+const emailData = require('./common/emailData')
 const app = express()
 
 //Middleware for bodyparser
@@ -11,33 +12,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //mongoDB configuration
-const db = require("./config/myURL").mongoURL; 
+const db = require("./config/myURL").mongoURL;
 
 
 //Attempt to connect to database
-
 mongoOptions = { useNewUrlParser: true, dbName: 'project_stack', autoIndex: false, useUnifiedTopology: true}
 mongoose
   .connect(db, mongoOptions)
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.log(err));
 
+//secretKey
 var secret = myURL.secret
 app.set('secretKey', secret)
 
 //templating engine
+// app.use(express.static(__dirname + 'views'))
 app.set('view engine', 'ejs')
+
 
 //logging APIs
 app.use(morgan('dev'))
-
+//EJS check on browser
+let emailObj = emailData.welcomeEmail('abcd')
+app.get('/render', (req, res) => {
+  res.render('basic',emailObj)
+})
 //routes
 app.use('/api', routes)
 
 
 
 
-
-
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 app.listen(port, () => console.log(`App is running at ${port}`));
